6860d51be0166a34452e13f1b97f8f3e48326ef2
Phaerya <joaquim.morais-faria@outlook.com>
2024-05-30 11:33:01 +0200

    add strapi url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+  url: env('BASE_URL', 'https://joaquim.moraisfaria.nantes.mds-project.fr/strapi/'),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
