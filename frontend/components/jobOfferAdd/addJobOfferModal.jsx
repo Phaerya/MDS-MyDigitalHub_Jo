81e5c6880d07f32fe8079be538d10e634be9f7db
Phaerya <joaquim.morais-faria@outlook.com>
2024-05-31 10:24:05 +0200

    jen ai marre

--- frontend/components/jobOfferAdd/addJobOfferModal.jsx
+++ frontend/components/jobOfferAdd/addJobOfferModal.jsx
@@ -3,10 +3,10 @@ import React, { useState } from 'react'
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, SelectItem, Select } from '@nextui-org/react'
 import { CREATE_OFFER } from '@/graphql/queries/queries'
 import client from '@/graphql/apolloClient'
-import offerData from '../../../backend/src/api/offer/content-types/offer/schema.json'
+// import offerData from '../../../backend/src/api/offer/content-types/offer/schema.json'
 
-const skillOptions = offerData.attributes.skills.options.map(skill => ({ label: skill, value: skill }))
-const educationOptions = offerData.attributes.education.options.map(edu => ({ label: edu, value: edu }))
+// const skillOptions = offerData.attributes.skills.options.map(skill => ({ label: skill, value: skill }))
+// const educationOptions = offerData.attributes.education.options.map(edu => ({ label: edu, value: edu }))
 
 export default function AddJobOfferModal () {
   const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure()
@@ -87,7 +87,7 @@ export default function AddJobOfferModal () {
           <ModalBody>
             <Input name='title' placeholder='Titre' value={formData.title} onChange={handleChange} />
             <Input name='description' placeholder='Description' value={formData.description} onChange={handleChange} />
-            <Select
+            {/* <Select
               items={skillOptions}
               label='Choix des compétences'
               placeholder='Compétences requises'
@@ -113,7 +113,7 @@ export default function AddJobOfferModal () {
                   {edu.label}
                 </SelectItem>
               ))}
-            </Select>
+            </Select> */}
             <Input name='start_date' placeholder='Date de début' type='date' value={formData.start_date} onChange={handleChange} />
           </ModalBody>
           <ModalFooter>
